66c8a670a96abee7e856ef02dd3e69b5fe7e5cf6
Bryan Brauchler <bbrauchl@umich.edu>
2023-11-16 23:54:23 -0500

    adding lualine and nvim-tree

---
+++ lua/config/plugins/nvim-tree.lua
@@ -0,0 +1,51 @@
+
+local my_on_attach = function(bufnr)
+  local api = require('nvim-tree.api')
+  
+  local opts = { 
+    buffer = bufnr,
+    noremap = true,
+    silent = true,
+    nowait = true,
+  }
+  
+  api.config.mappings.default_on_attach(bufnr)
+  
+  require('utils').mappings.load_mappings('nvim_tree_on_attach', opts)
+end
+
+local nvim_tree_config = {
+  'nvim-tree/nvim-tree.lua',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+  opts = {
+    sort_by = "case_sensitive",
+    on_attach = my_on_attach,
+    view = {
+      width = 30,
+    },
+    renderer = {
+      group_empty = true,
+    },
+    filters = {
+      dotfiles = true,
+    },
+  },
+  init = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    local opts = {
+      silent = true,
+      nowait = true,
+    }
+    require('utils').mappings.load_mappings("nvim_tree", opts)
+  end,
+  config = function(_, opts)
+    require("nvim-tree").setup(opts)
+  end,
+}
+
+return nvim_tree_config
