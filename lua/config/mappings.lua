26c421870f904e093963f4bb1f7aad3919230b66
Bryan Brauchler <bbrauchl@umich.edu>
2023-11-18 22:30:19 -0500

    Adding prefix to plugin keys and customize key mappings for telescope search

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,4 +1,3 @@
----@type MappingsTable
 --[[
 
 Create a unified way to set keymaps in a more general way.
@@ -90,27 +89,9 @@ M.general_mappings = {
 }
 
 M.plugin_mappings = {
+  -- [[ Configuration for nvim-tree ]]
   ['nvim-tree'] = {
-
     general = {
-      n = {
-        -- toggle
-        ["<C-n>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
-
-        -- focus
-        ["<leader>e"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvimtree" },
-      },
-    },
-    events = {
-
-    }
-
-  },
-}
-
-M.plugin_on_event_mappings = {
-  ['nvim-tree'] = {
-    global = {
       n = {
         -- toggle
         ["<C-n>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
@@ -122,12 +103,168 @@ M.plugin_on_event_mappings = {
     events = {
       on_attach = {
         n = {
-          ['<C-u>'] = { function() require('nvim-tree.api').tree.change_root_to_parent() end, "nvim-tree: Parent Directory" },
-          ['<C-o>'] = { function() require('nvim-tree.api').tree.change_root_to_node() end, "nvim-tree: Change Directory" },
+          ['<C-u>'] = {
+            function() require('nvim-tree.api').tree.change_root_to_parent() end,
+            "nvim-tree: Parent Directory"
+          },
+          ['<C-o>'] = {
+            function() require('nvim-tree.api').tree.change_root_to_node() end,
+            "nvim-tree: Change Directory"
+          },
         },
       },
     },
-  }
+  },
+
+  -- [[ Configuration for Telescope Plugin ]]
+  telescope = {
+    general = {
+      n = {
+        -- See `:help telescope.builtin`
+        ['<leader>/'] = {
+          function() require('telescope.builtin').current_buffer_fuzzy_find() end,
+          'Fuzzy Find'
+        },
+        ['<C-f>'] = {
+          function() require('telescope.builtin').current_buffer_fuzzy_find() end,
+          'Fuzzy Find'
+        },
+        ['<leader>fr'] = {
+          function() require('telescope.builtin').oldfiles() end,
+          '[F]ind [R]ecent Files'
+        },
+        ['<leader>fb'] = {
+          function() require('telescope.builtin').buffers() end,
+          '[F]ind Existing [B]uffers'
+        },
+        ['<leader>ff'] = {
+          function() require('telescope.builtin').find_files() end,
+          '[F]ind [F]iles',
+        },
+        ['<leader>fh'] = { function() require('telescope.builtin').help_tags() end, '[F]ind [H]elp' },
+        ['<leader>fw'] = { function() require('telescope.builtin').grep_string() end, '[F]ind current [W]ord' },
+        ['<leader>fG'] = { function() require('telescope.builtin').git_files() end, '[F]ind [G]it files' },
+        ['<leader>fg'] = {
+          function() require('telescope.builtin').live_grep(require('telescope.themes')) end,
+          '[F]ind by [G]rep'
+        },
+        ['<leader>fR'] = {
+          function() end, -- todo implement this
+          '[F]ind by grep on Git [R]oot'
+        },
+        ['<leader>fd'] = {
+          function() require('telescope.builtin').diagnostics() end, '[F]ind [D]iagnostics'
+        },
+        ['<leader>f<space>'] = { function() require('telescope.builtin').resume() end, '[F]ind (previous)' },
+      },
+    },
+    events = {
+
+    },
+  },
+
+  -- [[ barbar plugin keybinds ]]
+  barbar = {
+    general = {
+      n = {
+        ['<A-,>'] = {
+          '<cmd>BufferPrevious<cr>',
+          '[BarBar] Previous Buffer',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-.>'] = {
+          '<cmd>BufferNext<cr>',
+          '[BarBar] Next Buffer',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-1>'] = {
+          '<cmd>BufferGoTo 1<cr>',
+          '[BarBar] Buffer 1',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-2>'] = {
+          '<cmd>BufferGoTo 2<cr>',
+          '[BarBar] Buffer 2',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-3>'] = {
+          '<cmd>BufferGoTo 3<cr>',
+          '[BarBar] Buffer 3',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-4>'] = {
+          '<cmd>BufferGoTo 4<cr>',
+          '[BarBar] Buffer 4',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-5>'] = {
+          '<cmd>BufferGoTo 5<cr>',
+          '[BarBar] Buffer 5',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-6>'] = {
+          '<cmd>BufferGoTo 6<cr>',
+          '[BarBar] Buffer 6',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-7>'] = {
+          '<cmd>BufferGoTo 7<cr>',
+          '[BarBar] Buffer 7',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-8>'] = {
+          '<cmd>BufferGoTo 8<cr>',
+          '[BarBar] Buffer 8',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-9>'] = {
+          '<cmd>BufferGoTo 9<cr>',
+          '[BarBar] Buffer 9',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-0>'] = {
+          '<cmd>BufferLast<cr>',
+          '[BarBar] Last Buffer',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-p>'] = {
+          '<cmd>BufferPin<cr>',
+          '[BarBar] Toggle Pin Buffer',
+          opts = { noremap = true, silent = true }
+        },
+        ['<A-x>'] = {
+          '<cmd>BufferClose<cr>',
+          '[BarBar] Close Buffer',
+          opts = { noremap = true, silent = true }
+        },
+        ['<C-p>'] = {
+          '<cmd>BufferPick<cr>',
+          '[BarBar] Pick Buffer',
+          opts = { noremap = true, silent = true }
+        },
+        ['<leader>bb'] = {
+          '<cmd>BufferOrderByBufferNumber<cr>',
+          '[BarBar] Sort Buffers By Number',
+          opts = { noremap = true, silent = true }
+        },
+        ['<leader>bd'] = {
+          '<cmd>BufferOrderByDirectory<cr>',
+          '[BarBar] Sort Buffers By Directory',
+          opts = { noremap = true, silent = true }
+        },
+        ['<leader>bl'] = {
+          '<cmd>BufferOrderByLanguage<cr>',
+          '[BarBar] Sort Buffers By Language',
+          opts = { noremap = true, silent = true }
+        },
+        ['<leader>bw'] = {
+          '<cmd>BufferOrderByWindowNumber<cr>',
+          '[BarBar] Sort Buffers By Window Number',
+          opts = { noremap = true, silent = true }
+        },
+      },
+    },
+  },
 }
 
 return M
