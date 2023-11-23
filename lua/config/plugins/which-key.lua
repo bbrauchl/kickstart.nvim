db7ed1b0adfcfda473ac4ffb93255ad2472fa3c9
Bryan Brauchler <bbrauchl@umich.edu>
2023-11-23 07:54:57 -0500

    adding buffer close on :q

--- lua/config/plugins/which-key.lua
+++ lua/config/plugins/which-key.lua
@@ -1,7 +1,7 @@
 
 -- Useful plugin to show you pending keybinds.
-local which_key_config = { 
-  'folke/which-key.nvim', opts = {} 
+local which_key_config = {
+  'folke/which-key.nvim', opts = {}
 }
 
 return which_key_config
