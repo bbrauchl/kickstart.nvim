6ae1c537cb2727d7ac3a97f63b5d3ccc99522ffb
Bryan Brauchler <bbrauchl@umich.edu>
2023-12-20 11:34:10 -0500

    adjusting indent-blankline config

--- lua/config/plugins/indent-blankline.lua
+++ lua/config/plugins/indent-blankline.lua
@@ -8,15 +8,15 @@ local highlight = {
   "RainbowDelimiterViolet",
   "RainbowDelimiterCyan",
 }
---[[ local highlight = {
-      "RainbowRed",
-      "RainbowYellow",
-      "RainbowBlue",
-      "RainbowOrange",
-      "RainbowGreen",
-      "RainbowViolet",
-      "RainbowCyan",
-} ]]
+-- local highlight = {
+--       "RainbowRed",
+--       "RainbowYellow",
+--       "RainbowBlue",
+--       "RainbowOrange",
+--       "RainbowGreen",
+--       "RainbowViolet",
+--       "RainbowCyan",
+-- }
 
 local indent_blankline_config = {
   -- Add indentation guides even on blank lines
@@ -64,23 +64,31 @@ local indent_blankline_config = {
     -- show_first_indent_level = false,
     -- show_current_context = true,
   },
-  config = function()
-    local hooks = require('ibl.hooks')
-    --[[ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-    end) ]]
+  setup = function()
+    local status_ok, ibl = pcall(require, "ibl")
+    if not status_ok then
+      return
+    end
+    local status_ok, hooks = pcall(require, "ibl.hooks")
+    if not status_ok then
+      return
+    end
 
-    require('ibl').setup(
+    -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    --     vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    --     vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    --     vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    --     vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    --     vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    --     vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    --     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    -- end)
+
+    ibl.setup(
       require('config.plugins.indent-blankline').opts
     )
 
-    -- vim.g.rainbow_delimiters = {highlight = highlight}
+    vim.g.rainbow_delimiters = {highlight = highlight}
     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
   end,
 }
